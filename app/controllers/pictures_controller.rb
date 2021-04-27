68b44ba380e57405ea688c4064aec1efccd046c1
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-04-27 12:19:50 +0900

    画像投稿実装(完成)

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,10 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
   def index
     @pictures = Picture.all
   end
+
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -10,10 +12,12 @@ class PicturesController < ApplicationController
       @picture = Picture.new
     end
   end
+
   def confirm
     @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
+
   def create
     @picture = current_user.pictures.build(picture_params)
     if params[:back]
@@ -26,10 +30,13 @@ class PicturesController < ApplicationController
       end
     end
   end
+
   def show
   end
+
   def edit
   end
+
   def update
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿を編集しました！"
@@ -37,14 +44,18 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @picture.destroy
     redirect_to pictures_path, notice:"投稿を削除しました！"
   end
+
   private
+
   def picture_params
     params.require(:picture).permit(:image, :image_cache, :content)
   end
+
   def set_picture
     @picture = Picture.find(params[:id])
   end
